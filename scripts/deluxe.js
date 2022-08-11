3eb2967b4ed0cd9dbbb22e64148eeb42e2cabd92
Galen Wright-Watson <ww.galen@gmail.com>
2022-08-10 22:57:29 -0700

    Update: added `tabs` to deluxe module set.

--- scripts/deluxe.js
+++ scripts/deluxe.js
@@ -2,5 +2,5 @@
 	 * The sole purpose of this module is to load other modules through dependencies on them, shortening the query string for sheet.js.
 	 *
 	 * Auto-inclusion of the folowing modules:
-	 * @require abilities, edition, editor, health, notes
+	 * @require abilities, edition, editor, health, notes, tabs
 	 */
