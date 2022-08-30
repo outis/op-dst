dd45b92456d85d9a44cbd1d3071456d4e0ce0a42
Galen Wright-Watson <ww.galen@gmail.com>
2022-08-30 02:32:07 -0700

    Update: `shadow`-use `dsf`.

--- scripts/shadow.js
+++ scripts/shadow.js
@@ -1,9 +1,9 @@
 	/*** */
 	let shadow = globals.shadow = {
 		postLoad(opts, $context) {
-			let psyche_slug = $context.find('.dsf_psyche_slug').text();
+			let psyche_slug = dsf.value('psyche_slug');
 			if (psyche_slug) {
-				$context.find('.dsf_psyche').wrap(`<a href="/characters/${psyche_slug}"></a>`);
+				dsf.$dsf('psyche').wrap(`<a href="/characters/${psyche_slug}"></a>`);
 			}
 		}
 	};
