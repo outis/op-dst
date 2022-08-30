f4805226d2e5ceaada62e5897a5cb05fd36d1960
Galen Wright-Watson <ww.galen@gmail.com>
2022-08-29 23:45:33 -0700

    Addition: `abilities`-ability management (find & update).

--- scripts/abilities.js
+++ scripts/abilities.js
@@ -1,15 +1,119 @@
 	/**
+	 * Base for ability field proxies.
+	 */
+	class AbilityProxy {
+		constructor() {
+		}
+
+		get loaded() {
+			return abilities.loaded;
+		}
+
+		_get(field) {
+			return dsf.value(field) || dsa.data[field];
+		}
+		_set(field, value, fields) {
+			//if (fields.any(dsa.exists)) {
+			dsa.data[field] = value;
+			//}
+			if (dsf.exists(field)) {
+				let $field = dsf.$dsf(field);
+				if (pips.is($field, value)) {
+					return pips.value($field, value);
+				} else {
+					return dsf.value(field, value);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Represents a static ability field.
+	 */
+	class StaticAbility extends AbilityProxy {
+		constructor(name) {
+			super();
+
+			Object.defineProperty(this, 'name', {
+				value: name
+			});
+		}
+		get specialty() {
+			return this._get(`${this.name}_specialty`);
+		}
+		set specialty(value) {
+			this._set(`${this.name}_specialty`, value);
+		}
+		get value() {
+			return this._get(this.name);
+		}
+		set value(value) {
+			pips.value(dsf.$dsf(this.name), value);
+		}
+	}
+
+	/**
+	 * Represents a dynamic (UDF) ability field.
+	 */
+	class DynamicAbility extends AbilityProxy {
+		constructor(data) {
+			super();
+
+			Object.defineProperty(this, 'data', {
+				value: data,
+			});
+		}
+
+		get ability() {
+			return this.data.base;
+		}
+		get base() {
+			return this.data.base;
+		}
+		get i() {
+			return this.data.i;
+		}
+		set i(value) {
+			this.data.i = value;
+		}
+		get name() {
+			return this._get(this.data.name_field);
+		}
+		set name(value) {
+			this._set(this.data.name_field, value);
+		}
+		get specialty() {
+			return this._get(this.data.specialty_field);
+		}
+		set specialty(value) {
+			this._set(this.data.specialty_field, value);
+		}
+		get value() {
+			return this._get(this.data.value_field);
+		}
+		set value(value) {
+			pips.value(dsf.$dsf(this.data.value_field), value);
+		}
+	};
+
+	/**
 	 * Extracts specialties from abilities.
 	 *
 	 * TODO: add management methods (so as to prevent duplicate dynamic abilities & ease altering them).
 	 */
 	const abilities = globals.abilities = {
 		abilities: ['talents', 'skills', 'knowledges'],
+		// should probably be in a separate class
+		_dynamic: {},
 
 		/* DST event handlers */
 		preLoad() {
 			module.tarryFor('compatibility'); // so dynamic abilities have been imported
 			this.separateSpecialties();
+		},
+
+		postLoad() {
+			module.on('postLoad', () => this.loaded = true, 'abilities.postLoad');
 		},
 
 		/**
@@ -24,6 +128,35 @@
 		},
 
 		/* */
+		find(name, base) {
+			name = this.normalizeName(name);
+			if (dsf.exists(name)) { // static
+				return new StaticAbility(name);
+			} else { // dynamic
+				if (this._dynamic[name]) {
+					if (name == this.normalizeName(this._dynamic[name].name)) {
+						return this._dynamic[name];
+					}
+					// dynamic ability no longer matches its key, so remove it
+					delete this._dynamic[name];
+				}
+				this.scan();
+				if (this._dynamic[name]) {
+					return this._dynamic[name];
+				}
+			}
+		},
+
+		normalizeName(name) {
+			return this.fieldName(name).replace(/\W+/, '');
+		},
+
+		scan() {
+			for (let data of udf.scan('abilities')) {
+				let name = this.normalizeName(data.name);
+				this._dynamic[name] = new DynamicAbility(data);
+			}
+		},
 
 		separateSpecialty(tpls, env, name, value) {
 			//value ??= dsa.data[name] ?? '';
